6b4e15438e39041f348348abbdf5ba0255e97792
Oleksandr Kitaika <kitayka96@gmail.com>
2020-04-05 22:06:06 +0300

    hw1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
-var name = prompt("Enter your name, please:");
-var years = prompt("How old are you?");
-alert("Hello, " + name + ". You are " + years + " years old.");
+let name = prompt("Enter your name, please:");
+let years = prompt("What year were you born in?");
+let today = new Date();
+let todayYear = today.getFullYear();
+alert("Hello, " + name + "! You are " + (todayYear - years) + " years old.");
