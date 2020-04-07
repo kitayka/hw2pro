97954895b30ff056b761ed3350dc4f583e593f29
Oleksandr Kitaika <kitayka96@gmail.com>
2020-04-08 01:35:12 +0300

    hw2pro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
-let name = prompt("Enter your name, please:");
-let years = prompt("What year were you born in?");
+let name = prompt("Пожалуйста, введите свое имя:");
+let years = prompt("Введите год вашего рождения:");
 let today = new Date();
 let todayYear = today.getFullYear();
-alert("Hello, " + name + "! You are " + (todayYear - years) + " years old.");
+let quantityYears = (todayYear - years);
+let caseYear;
+if (quantityYears % 10 >= 2 && quantityYears % 10 <= 4) {
+    caseYear = " года";
+}  else if (quantityYears >= 5 && quantityYears <= 20) {
+    caseYear = " лет";
+}  else if (quantityYears % 10 == 1) {
+    caseYear = " год";
+}  else {
+    caseYear = " лет";
+}
+alert("Привет, " + name + "! Тебе " + quantityYears + caseYear + "!");
